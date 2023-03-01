2e8d2608ab2b4cc08074a36628c0120bab4c0349
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-03-01 23:57:09 +0530

    add html skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" />
+    </header>
+    <div class="options-container"></div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  min-width: 1080px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+}
+</style>
