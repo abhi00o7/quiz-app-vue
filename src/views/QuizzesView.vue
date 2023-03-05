1d4b9e7c0df5a3118cde563828ce9c69f5f6c7c8
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-03-06 03:51:07 +0530

    add animations

--- src/views/QuizzesView.vue
+++ src/views/QuizzesView.vue
@@ -3,6 +3,7 @@ import quiz from "../data/quizData.json";
 import { ref, watch } from "vue";
 import Card from "../components/Card.vue";
 import NotFound from "../components/NotFound.vue";
+import gsap from "gsap";
 
 const quizzes = ref(quiz);
 const search = ref("");
@@ -12,20 +13,49 @@ watch(search, () => {
     element.name.toLowerCase().includes(search.value.toLowerCase())
   );
 });
+
+// transition animation functions
+const beforeEnter = (el) => {
+  // card-enter-from
+  el.style.transform = `translateY(-50px)`;
+  el.style.opacity = 0;
+};
+const enter = (el) => {
+  // card-enter-to
+  gsap.to(el, {
+    y: 0,
+    opacity: 1,
+    duration: 0.5,
+    delay: el.dataset.index * 0.4,
+  });
+};
 </script>
 
 <template>
-    <header>
-      <h1>Quizzes</h1>
-      <input type="text" v-model.trim="search" placeholder="Search Here" />
-    </header>
-    <div class="options-container">
-      <NotFound
-        :msg="'Item not found! Please try using different keywords.'"
-        v-show="!quizzes.length"
+  <header>
+    <h1>Quizzes</h1>
+    <input type="text" v-model.trim="search" placeholder="Search Here" />
+  </header>
+  <div class="options-container">
+    <NotFound
+      :msg="'Item not found! Please try using different keywords.'"
+      v-show="!quizzes.length"
+    />
+    <TransitionGroup
+      name="card"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      appear
+    >
+      <Card
+        v-for="(subject, index) in quizzes"
+        :key="subject.id"
+        :subject="subject"
+        :data-index="index"
       />
-      <Card v-for="subject in quizzes" :key="subject.id" :subject="subject" />
-    </div>
+    </TransitionGroup>
+  </div>
 </template>
 
 <style scoped>
@@ -48,8 +78,21 @@ header h1 {
 
 header input {
   border: none;
-  background-color: rgba(128, 128, 128, 0.1);
+  background-color: rgba(255 255 255/0.09);
+  color: ghostwhite;
   padding: 10px;
   border-radius: 5px;
+}
+
+.card-enter-from {
+  transform: translateY(-50px);
+  opacity: 0;
+}
+.card-enter-to {
+  transform: translateY(0);
+  opacity: 1;
+}
+.card-enter-active {
+  transition: all 0.5s ease;
 }
 </style>
